b473df48a5b27fc4b66bea178ff7a600598c8036
dewwta <delltaboltz@gmail.com>
2023-11-30 04:25:40 -0500

    V0.02 --Unstable

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -32,11 +32,6 @@ ebr_volume_id:              db 12h, 34h, 56h, 78h           ; serial number, val
 ebr_volume_label:           db 'MAGE OS'                ; 7 Bytes padded with spaces
 ebr_system_id:              db 'FAT12'                      ; 8 bytes
 
-;
-; code goes here
-;
-
-
 start:
     jmp main
 ;
@@ -77,15 +72,42 @@ main:
     mov ss, ax
     mov sp, 0x7C00       ; stack grows downwards from where we are loaded in memory
 
+    ; read something from floppy disk
+    ; BIOS should be set DL to drive number
+    mov [ebr_drive_number], dl
+
+    mov ax, 1               ; LBA=1, second sector from disk
+    mov cl, 1               ; 1 sector to read
+    mov bx, 0x7E00          ; data should be after the bootloader
+    call disk_read
+
+
     ; print message
     mov si, msg_hello
     call puts
-
     
+    cli
     hlt
 
+;
+; Error Handlers
+;
+
+
+
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h                             ; wait for keypress
+    jmp 0FFFFh:0                        ; jump to beginning of BIOS, should reboot
+    
 .halt:
-    jmp .halt
+    cli                                 ; disable interrupts, this way CPU can't get out of "halt" state
+    hlt                                 
 
 ;
 ; Disk Routines
@@ -102,6 +124,9 @@ main:
 
 lba_to_chs:
 
+    push ax
+    push dx
+
     xor dx, dx                          ; dx = 0
     div word [bdb_sectors_per_track]    ; ax = LBA / SectorsPerTrack
                                         ; dx = LBA % SectorsPerTrack
@@ -112,13 +137,83 @@ lba_to_chs:
     xor dx, dx                          ; dx = 0
     div word[bdb_heads]                 ; ax = (LBA / SectorsPerTrack) / Heads
                                         ; dx = (LBA / SectorsPerTrack) % Heads
+    mov dh, dl                          ; dh = head 
+    mov ch, al                          ; ch = cylinder (lower 8 bits)
+    shl ah, 6
+    or cl, ah                           ; put upper 2 buts of cylinder in CL
+
+    pop ax
+    mov dl, al                          ; restore DL
+    pop ax
+    ret
+
+;
+; Reads sectors
+; Parameters:
+;   - ax: LBA address
+;   - cl: number of sectors to read (up to 128)
+;   - dl: drive number
+;   - es:bx: memory address where to store and read data
+;
+disk_read:
+    
+    push ax                             ; save registers we will modify
+    push bx
+    push cx
+    push dx
+    push di
+    
+    
+    push cx                             ; temporarily save CL (number of sectors to read)
+    call lba_to_chs                     ; compute CHS
+    pop ax                              ; AL = number of sectors to read
+
+    mov ah, 02h
+    mov di, 3                           ; retry count
+
+.retry:
+    pusha
+    stc
+    int 13h
+    jnc .done
+
+    ; read failed
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; after all attempts are exhausted
+    jmp floppy_error
+.done:
+    popa
+
+    pop di                         ; restore registers modified
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
+;
+; Resets disk controller
+; Parameters:
+; dl: drive number
+;
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
 
 
-
-
-
-
-msg_hello: db 'Hello world!', ENDL, 0
-
+msg_hello:                  db 'Hello world!', ENDL, 0
+msg_read_failed:            db 'Read from disk failed!', ENDL, 0
 times 510-($-$$) db 0
 dw 0AA55h
